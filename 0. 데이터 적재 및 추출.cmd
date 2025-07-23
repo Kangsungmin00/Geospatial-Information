41b6757aa0ee91f62224b5a70e5beceaa8b41fcc
Kangsungmin00 <kangsm0218@gmail.com>
2025-07-23 12:09:46 +0900

    Update 0. 데이터 적재 및 추출.cmd

--- 0. 데이터 적재 및 추출.cmd
+++ 0. 데이터 적재 및 추출.cmd
@@ -3,7 +3,7 @@ cd C:\Program Files\QGIS 3.40.7\bin
 psql -U postgres -d 데이터베이스명
 
 # OSGeo4w Shell에서 실행
-ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=데이터베이스명 password=비밀번호" "파일 경로.shp" -nln 테이블명 -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=geom -oo ENCODING=CP949 -a_srs EPSG:5186 -overwrite -progress
+ogr2ogr -f "PostgreSQL" PG:"host=localhost user=postgres dbname=데이터베이스명 password=비밀번호" "파일 경로.shp" -nln 테이블명 -nlt PROMOTE_TO_MULTI -lco GEOMETRY_NAME=geom --config SHAPE_ENCODING CP949 -a_srs EPSG:5186 -overwrite -progress
 
 
 # shp 파일로 데이터 내보내기
